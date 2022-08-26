0d10f9405f551685c7ee6d41805d83e8879dbad9
Nijat Muhtar <nijatmn@gmail.com>
2022-08-26 14:53:50 -0400

    method inplemented

--- src/test/java/practice/MethodPracticeTest.java
+++ src/test/java/practice/MethodPracticeTest.java
@@ -11,5 +11,17 @@ public class MethodPracticeTest {
     public void testCase() {
 
     }
+
+    public String isItHotOut(int temp) {
+        if(temp >= 90) {
+            return "You should wear lightly! its hot out there yo!";
+        }
+        else if(temp < 90 && temp >= 80) {
+            return "You can just wear casually! Its nice out~!";
+        }
+        else {
+            return "Put on a jacket, just in case!";
+        }
+    }
 }
 // the end of the class
