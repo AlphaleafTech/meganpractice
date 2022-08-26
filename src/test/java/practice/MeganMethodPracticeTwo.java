486d155eccda5f4f3c0a758cd820205da3b17ed0
Nijat Muhtar <nijatmn@gmail.com>
2022-08-26 15:36:03 -0400

    new challenge to megan

---
+++ src/test/java/practice/MeganMethodPracticeTwo.java
@@ -0,0 +1,24 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class MeganMethodPracticeTwo {
+
+    @Test
+    public void testCase() {
+
+    }
+
+
+    /**
+     * I am going to give you a two number
+     *    height -> height of the triangle
+     *    base   -> base length of the triangle
+     *
+     *  area = (height * base)/2
+     */
+    public int areaOfTriangle(int height, int base) {
+
+        return 0;
+    }
+}
--- src/test/java/practice/MethodPracticeTest.java
+++ src/test/java/practice/MethodPracticeTest.java
@@ -9,8 +9,15 @@ public class MethodPracticeTest {
 // message from megan
     @Test
     public void testCase() {
-        String ret = isItHotOut(81);
-        System.out.println(ret);
+//        String ret = isItHotOut(81);
+//        System.out.println(ret);
+
+        int[] numDatabase = getNumData();
+
+        for(int i = 0; i < numDatabase.length; i++) {
+            int each = numDatabase[i];
+            isLuckyNumber(each);
+        }
 
     }
 
@@ -23,9 +30,8 @@ public class MethodPracticeTest {
     public void isLuckyNumber(int number) {
         int result = number % 7;
         if(result ==0 ) {
-            System.out.println(" That is a lucky number!");
+            System.out.println("\t" +number + "\tis a lucky number!!");
         }
-
     }
 
     //
@@ -40,6 +46,15 @@ public class MethodPracticeTest {
             return"Put on a jacket, just in case!";
         }
 
+    }
+
+    public int[] getNumData() {
+        int[] temp = new int[10_000];
+        for(int i =0; i < temp.length; i++) {
+            int rand = (int)(Math.random() * 9999) + 1;
+            temp[i] = rand;
+        }
+        return temp;
     }
 }
 // the end of the class
